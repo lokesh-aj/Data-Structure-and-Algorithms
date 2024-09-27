4f7ca44108b589ae56542cb0560ffceffc39e7b7
Lokesh Vyas <lokeshvyas.prof@gmail.com>
2024-09-27 22:06:37 +0530

    Update LL.java

--- LL.java
+++ LL.java
@@ -1,195 +1,180 @@
+import java.util.Scanner;
+
 /**
- * A class representing a singly linked list. It allows adding nodes
- * at the beginning and the end, deleting nodes from the beginning
- * and the end, printing the list, and retrieving the size of the list.
+ * This class represents a singly linked list (LL) structure.
+ * It allows adding elements to the front and back of the list,
+ * deleting the first and last elements, and traversing the list.
  */
 class LL {
+    Node head; // Pointer to the head (first) node of the linked list
+    Node ptr; // Pointer used for traversing the list
+    Node preptr; // Pointer used to store the previous node during traversal
 
-    // Reference to the first node (head) of the linked list
-    Node head;  
-    
-    // Reference to the last node (tail) of the linked list
-    Node tail;  
-    
-    // Keeps track of the number of nodes in the linked list
-    int size = 0;  
+    int size = 0; // Keeps track of the size of the list
 
     /**
-     * A class representing a node in the linked list.
-     * Each node contains data and a reference to the next node.
+     * This class represents a single node of the linked list.
      */
     class Node {
-        String data;  // Data stored in the node
-        Node next;    // Reference to the next node in the linked list
+        String data; // Data to store in the node
+        Node next; // Pointer to the next node in the list
 
         /**
-         * Constructor to create a new node with the given data.
-         * 
-         * parameter _data The data to be stored in the node.
+         * Constructor to create a new node with given data.
+         * @param _data The data to be stored in the node
          */
-        Node(String _data) {
-            this.data = _data;  // Initialize the data of the node
-            this.next = null;   // By default, next is set to null
-            size++;  // Increase the size of the linked list when a node is created
+        public Node(String _data) {
+            this.data = _data;
+            this.next = null;
+            size++; // Increase size when a new node is created
         }
     }
 
     /**
-     * Adds a node with the specified data at the beginning of the linked list.
-     * 
-     * parameter data The data to be stored in the new node.
+     * Adds a new node with the given data at the front of the list.
+     * @param data The data to be added to the list
      */
     public void addFirst(String data) {
-        Node newnode = new Node(data);  // Create a new node with the provided data
+        Node newnode = new Node(data);
 
+        // If the list is empty, make the new node the head
         if (head == null) {
-            System.out.println("Head -> Null");
             head = newnode;
-            tail = newnode;  // Since it's the first node, head and tail point to the same node
             return;
         }
 
-        newnode.next = head;  // Link the new node to the current head
-        head = newnode;  // Update head to the new node
+        // New node points to the current head and becomes the new head
+        newnode.next = head;
+        head = newnode;
     }
 
     /**
-     * Adds a node with the specified data at the end of the linked list.
-     * 
-     * parameter data The data to be stored in the new node.
+     * Adds a new node with the given data at the end of the list.
+     * @param data The data to be added to the list
      */
     public void addLast(String data) {
-        Node newnode = new Node(data);  // Create a new node with the provided data
+        Node newnode = new Node(data);
 
-        if (head == null) {  // If the list is empty, initialize both head and tail
-            System.out.println("Tail -> Null");
-            head = newnode;
-            tail = newnode;
-            return;
-        }
-
-        tail.next = newnode;  // Link the current tail's next to the new node
-        tail = newnode;  // Update tail to the new node
-    }
-
-    /**
-     * Prints the linked list starting from the head.
-     * Outputs the elements in the format: Head -> [data1] -> [data2] -> ... -> Null.
-     */
-    public void printList() {
+        // If the list is empty, make the new node the head
         if (head == null) {
-            System.out.println("The List is empty.");
+            head = newnode;
             return;
         }
 
-        Node currentNode = head;  // Start from the head of the list
-        System.out.print("Head -> ");
-
-        while (currentNode != null) {  // Traverse the list and print each node
-            System.out.print(currentNode.data + " -> ");
-            currentNode = currentNode.next;  // Move to the next node
+        // Traverse the list to find the last node
+        ptr = head;
+        while (ptr.next != null) {
+            ptr = ptr.next;
         }
-        System.out.println("Null");  // Indicate the end of the list
+
+        // Append the new node at the end of the list
+        ptr.next = newnode;
     }
 
     /**
-     * Deletes the first node (head) from the linked list.
-     * If the list is empty, prints a message indicating so.
+     * Traverses the list and prints each node's data.
+     */
+    public void traverseList() {
+        // If the list is empty, display a message
+        if (head == null) {
+            System.out.println("List is Empty");
+            return;
+        }
+
+        // Start from the head and print each node's data
+        ptr = head;
+        while (ptr != null) {
+            System.out.print(ptr.data + " -> ");
+            ptr = ptr.next;
+        }
+        System.out.println("Null"); // Indicate the end of the list
+    }
+
+    /**
+     * Deletes the first node (head) of the list.
      */
     public void deleteFirst() {
+        // If the list is empty, display a message
         if (head == null) {
-            System.out.println("The List is empty.");
+            System.out.println("List is Empty");
             return;
         }
 
-        head = head.next;  // Move head to the next node
-        size--;  // Decrease the size of the list
-
-        // If the list becomes empty, update tail to null
-        if (head == null) {
-            tail = null;
-        }
+        // Move the head pointer to the next node
+        head = head.next;
+        size--; // Decrease the size as the first node is deleted
     }
 
     /**
-     * Deletes the last node (tail) from the linked list.
-     * If the list is empty, prints a message indicating so.
+     * Deletes the last node of the list.
      */
     public void deleteLast() {
+        // If the list is empty, display a message
         if (head == null) {
-            System.out.println("Empty List, nothing to delete");
+            System.out.println("List is Empty");
             return;
         }
 
-        size--;  // Decrease the size of the list
+        ptr = head;
+        preptr = head;
 
-        // If there's only one node, set both head and tail to null
-        if (head.next == null) {
-            head = null;
-            tail = null;
-            return;
+        // Traverse the list to find the last node
+        while (ptr.next != null) {
+            ptr = ptr.next;
         }
 
-        // Traverse to the second last node
-        Node currNode = head;
-        while (currNode.next != tail) {
-            currNode = currNode.next;
+        // Traverse again to find the second last node
+        while (preptr.next != ptr) {
+            preptr = preptr.next;
         }
 
-        // Update tail to the second last node and remove the last node
-        tail = currNode;
-        tail.next = null;
+        // Disconnect the last node
+        preptr.next = null;
+        size--; // Decrease the size as the last node is deleted
     }
 
     /**
-     * Prints the size of the linked list.
+     * Prints the size of the list.
      */
     public void getSize() {
-        System.out.println("Size of the Linked list: " + size);
+        System.out.println("Size : " + size);
     }
 
     /**
-     * Main method to test the functionality of the linked list class.
-     * Demonstrates adding, deleting, and printing nodes in the linked list.
-     * 
+     * The main method to test the linked list operations.
+     * @param args Command-line arguments (not used)
      */
-    public static void main(String args[]) {
-        LL list = new LL();  // Create a new linked list
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        // Adding nodes to the front of the list
-        list.addFirst("Linked List");
-        list.printList();  // Print the current list
-        list.getSize();  // Print the current size
+        // Create an empty linked list
+        LL list = new LL();
 
-        list.addFirst("a");
-        list.printList();
+        // Perform various operations on the linked list
+        list.traverseList();
         list.getSize();
+        list.addFirst("d");
 
-        list.addFirst("is");
-        list.printList();
+        list.traverseList();
         list.getSize();
+        list.addFirst("c");
 
-        list.addFirst("This");
-        list.printList();
+        list.traverseList();
         list.getSize();
+        list.addFirst("b");
 
-        // Deleting nodes from the front of the list
+        list.traverseList();
+        list.getSize();
+        list.addLast("a");
+
+        list.traverseList();
+        list.getSize();
         list.deleteFirst();
-        list.printList();
-        list.getSize();
 
-        list.deleteFirst();
-        list.printList();
+        list.traverseList();
         list.getSize();
-
-        // Adding nodes to the end of the list
-        list.addLast("new last");
-        list.printList();
-        list.getSize();
-
-        // Deleting the last node
         list.deleteLast();
-        list.printList();
+        list.traverseList();
         list.getSize();
     }
 }
